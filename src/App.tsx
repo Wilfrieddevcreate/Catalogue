9ed38de7844018929bea523344a7a07a2328cb7b
Wilfrieddevcreate <yves.heloussato@epitech.eu>
2024-09-13 18:16:46 +0100

    Add correction des routes de partage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/Scrolltop";
 import Home from "./pages/home";
 import Cart from "./pages/Cart"
-import ShareButton from "./pages/gesr";
 import ProductDetailPage from "./pages/ProductDetail";
 import CategorieProducts from "./pages/cateogorieProduct";
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/panier" element={<Cart />} />
-          <Route path="/share" element={<ShareButton />} />
           <Route path={`/detail/:slug`} element={<ProductDetailPage />} />
           <Route path={`/categorie/:categoryName`} element={<CategorieProducts />} /> 
         </Routes>
